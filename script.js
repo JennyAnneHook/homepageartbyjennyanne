4610cb65c1e4cf516107a9e1a38142fd0142399b
Jenny Anne Höök <93882622+JennyAnneHook@users.noreply.github.com>
2023-02-11 11:10:34 +0100

    Main

---
+++ script.js
@@ -0,0 +1,19 @@
+
+ //* This code is broken, I guess it is something wrong with secureToken, no connection to my mail will check this later -->
+//     <!-- <script src="https://smtpjs.com/v3/smtp.js"></script> 
+//     <script>
+//         function sendEmail(){
+            
+//             Email.send({
+//             secureToken: "dd499447-d518-4ad5-8e97-4e4998ee5652",
+//             To : 'jenny@artbyjennyanne.se',
+//             From : document.getElementById("email").value,
+//             Subject : "New Contact Form Enquiry",
+//             Body : "Name: " + document.getElementById("name").value
+//             + "<br> Email: " + document.getElementById("email").value
+//             + "<br> Message" + document.getElementById("message").value
+//         }).then(
+//   message => alert("Meddelandet har skickats")
+// );
+//         }
+//     </script>  -->
